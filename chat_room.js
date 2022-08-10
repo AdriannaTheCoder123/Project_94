e28a52d2216fa1f97f19f03f584928846b8e3134
AdriannaTheCoder123 <100245932+AdriannaTheCoder123@users.noreply.github.com>
2022-08-10 10:46:29 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDMUKjZ0K-q4Da3xF7hI9BvUvu1YcACq_g",
+    authDomain: "let-s-chat-e45e3.firebaseapp.com",
+    projectId: "let-s-chat-e45e3",
+    storageBucket: "let-s-chat-e45e3.appspot.com",
+    messagingSenderId: "366541036870",
+    appId: "1:366541036870:web:b250b3267b437f3bf1d947"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
